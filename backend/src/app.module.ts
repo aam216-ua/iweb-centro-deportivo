96f3e415729889502e5361c019e02b015a4c00f2
hhb1-ua <hhb1@alu.ua.es>
2024-12-19 23:31:25 +0100

    Connection to database

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,11 +3,18 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
-    ServeStaticModule.forRoot({
-      rootPath: join(__dirname, 'client/dist'),
+    ServeStaticModule.forRoot({ rootPath: join(__dirname, 'public') }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: 5432,
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
     }),
   ],
   controllers: [AppController],
