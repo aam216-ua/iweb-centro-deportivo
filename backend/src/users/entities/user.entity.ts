a59fb159907902cc34725b2abc4629c94c654006
hhb1-ua <hhb1@alu.ua.es>
2024-12-20 00:08:32 +0100

    Created model

--- backend/src/users/entities/user.entity.ts
+++ backend/src/users/entities/user.entity.ts
@@ -1,1 +1,34 @@
-export class User {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+export enum UserRole {
+  SUPERADMIN = 'super',
+  RECEPTIONIST = 'receptionist',
+  CUSTOMER = 'customer',
+}
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'varchar' })
+  email: string;
+
+  @Column({ type: 'varchar' })
+  name: string;
+
+  @Column({ type: 'varchar' })
+  surname: string;
+
+  @Column({ type: 'varchar' })
+  phone: string;
+
+  @Column({ type: 'enum', enum: UserRole, default: UserRole.CUSTOMER })
+  role: UserRole;
+
+  @Column({ type: 'boolean' })
+  active: boolean;
+
+  @Column({ type: 'money' })
+  balance: number;
+}
