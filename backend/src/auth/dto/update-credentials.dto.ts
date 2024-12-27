bf20549f7d36682f4bb5e6e3d6bd2ad817a71f52
hhb1-ua <hhb1@alu.ua.es>
2024-12-27 19:02:07 +0100

    Updated password validation requirements

--- backend/src/auth/dto/update-credentials.dto.ts
+++ backend/src/auth/dto/update-credentials.dto.ts
@@ -3,11 +3,11 @@ import { IsStrongPassword, MaxLength } from 'class-validator';
 export class UpdateCredentialsDto {
   @IsStrongPassword({
     minLength: 8,
-    minNumbers: 0,
-    minSymbols: 0,
-    minLowercase: 0,
-    minUppercase: 0,
+    minNumbers: 1,
+    minSymbols: 1,
+    minLowercase: 1,
+    minUppercase: 1,
   })
-  @MaxLength(256)
+  @MaxLength(64)
   newPassword: string;
 }
