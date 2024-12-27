5d7b2d78c1bdaee6707ef6ff14bd92f6eb91792a
hhb1-ua <hhb1@alu.ua.es>
2024-12-27 17:51:31 +0100

    User update fixes

--- backend/src/users/dto/update-user.dto.ts
+++ backend/src/users/dto/update-user.dto.ts
@@ -1,6 +1,6 @@
-import { OmitType, PartialType } from '@nestjs/mapped-types';
+import { PartialType, PickType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
 
 export class UpdateUserDto extends PartialType(
-  OmitType(CreateUserDto, ['email', 'name', 'surname', 'phone'])
+  PickType(CreateUserDto, ['email', 'name', 'surname', 'phone'] as const)
 ) {}
