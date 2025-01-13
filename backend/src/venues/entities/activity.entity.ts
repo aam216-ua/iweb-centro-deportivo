25ef945c7ae738e7b5d1dffe53a036f222c014b0
Rubén Del Castillo Fuentes <rdcf1@alu.ua.es>
2025-01-13 11:51:50 +0100

    Documentación API pistas

--- backend/src/venues/entities/activity.entity.ts
+++ backend/src/venues/entities/activity.entity.ts
@@ -1,14 +1,29 @@
 import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 import { Venue } from './venue.entity';
+import { ApiProperty } from '@nestjs/swagger';
 
 @Entity()
 export class Activity {
+  @ApiProperty({
+    description: 'Identificador único de la actividad',
+    type: 'string',
+    example: '123e4567-e89b-12d3-a456-426614174000',
+  })
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @ApiProperty({
+    description: 'Nombre de la actividad',
+    type: 'string',
+    example: 'football',
+  })
   @Column({ type: 'varchar' })
   name: string;
 
+  @ApiProperty({ 
+    description: 'Listado de pistas de la actividad',
+    type: 'array',
+  })
   @OneToMany(() => Venue, (venue) => venue.activity, {
     onDelete: 'RESTRICT',
   })
